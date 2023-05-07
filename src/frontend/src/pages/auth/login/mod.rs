22424a493b75c88fced0f70257680971b8fcdc75
vilayat-ali <vilayatcodemysite@gmail.com>
2023-05-07 23:56:13 +0530

    Added login page designs

--- src/frontend/src/pages/auth/login/mod.rs
+++ src/frontend/src/pages/auth/login/mod.rs
@@ -1,12 +1,83 @@
-use yew::prelude::{function_component, html, Html};
+use yew::{
+    prelude::{function_component, html, Html},
+    Callback,
+};
 
 use crate::layout::website::WebsiteLayout;
 
+struct LoginDetails {
+    email: Option<String>,
+    password: Option<String>,
+}
+
+impl LoginDetails {
+    fn new() -> Self {
+        Self {
+            email: None,
+            password: None,
+        }
+    }
+}
+
 #[function_component]
 pub fn Login() -> Html {
+    // state
+    let email = use_state(|| String::new());
+    let password = use_state(|| String::new());
+
+    let handle_email = Callback::from(move |_| {
+        let email = email.clone();
+        email.set(String::from("New"))
+    });
+
+    let handle_password = Callback::from(move |_| {
+        let password = password.clone();
+        password.set(String::from("Newly"))
+    });
+
     html! {
         <WebsiteLayout>
-            <h1>{"Login cange"}</h1>
+            <div class="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
+            <header class="max-w-lg mx-auto">
+                <a href="#">
+                    <h1 class="text-4xl font-bold text-white text-center">{"Startup"}</h1>
+                </a>
+            </header>
+
+            <main class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+                <section>
+                    <h3 class="font-bold text-2xl">{"Welcome to Startup"}</h3>
+                    <p class="text-gray-600 pt-2">{"Sign in to your account."}</p>
+                </section>
+
+                <section class="mt-10">
+                    <form class="flex flex-col" method="POST" action="#">
+                        <div class="mb-6 pt-3 rounded bg-gray-200">
+                            <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">{"Email"}</label>
+                            <input type="text" id="email" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" onchange={handle_email} />
+                        </div>
+                        <div class="mb-6 pt-3 rounded bg-gray-200">
+                            <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">{"Password"}</label>
+                            <input type="password" id="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" onchange={handle_password} />
+                        </div>
+                        <div class="flex justify-end">
+                            <a href="#" class="text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6">{"Forgot your password?"}</a>
+                        </div>
+                        <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">{"Sign In"}</button>
+                    </form>
+                </section>
+            </main>
+
+            <div class="max-w-lg mx-auto text-center mt-12 mb-6">
+                <p class="text-white">{"Don't have an account? "}<a href="#" class="font-bold hover:underline">{"Sign up"}</a>{"."}</p>
+            </div>
+
+            <footer class="max-w-lg mx-auto flex justify-center text-white">
+                <a href="#" class="hover:underline">{"Contact"}</a>
+                <span class="mx-3">{"•"}</span>
+                <a href="#" class="hover:underline">{"Privacy"}</a>
+            </footer>
+        </div>
         </WebsiteLayout>
     }
 }
