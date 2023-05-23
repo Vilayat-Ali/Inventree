8a2146f53df727c94cae62f8f1ae8917a9fcaaa0
vilayat-ali <vilayatcodemysite@gmail.com>
2023-05-23 15:38:14 +0530

    Added code for dashboard mapping of menu items in the sidebar

--- src/frontend/src/pages/dashboard/mod.rs
+++ src/frontend/src/pages/dashboard/mod.rs
@@ -3,9 +3,26 @@ use yewdux::prelude::*;
 
 use crate::{pages::auth::AuthProtected, State};
 
+struct MenuItem {
+    title: String,
+    href: String,
+}
+
+impl MenuItem {
+    fn new(title: String, href: String) -> Self {
+        Self { title, href }
+    }
+}
+
 #[function_component]
 pub fn Dashboard() -> Html {
     let state = use_store_value::<State>();
+
+    let dashboard_menu_items: Vec<MenuItem> = vec![
+        MenuItem::new("Item 1".to_string(), "".to_string()),
+        MenuItem::new("Item 2".to_string(), "".to_string()),
+        MenuItem::new("Item 3".to_string(), "".to_string()),
+    ];
 
     html! {
       <AuthProtected is_authorized={!state.is_user_authorized}>
@@ -15,7 +32,7 @@ pub fn Dashboard() -> Html {
               <div class="flex flex-wrap items-center">
                   <div class="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white">
                       <a href="#" aria-label="Home">
-                          <span class="text-xl pl-2"><i class="em em-grinning"></i></span>
+                          <span class="text-xl pl-2">{"Dashboard"}</span>
                       </a>
                   </div>
                   <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-white px-2">
@@ -60,6 +77,15 @@ pub fn Dashboard() -> Html {
                   <div class="bg-gray-800 shadow-xl h-20 fixed bottom-0 mt-12 md:relative md:h-screen z-10 w-full md:w-48 content-center">
                       <div class="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
                           <ul class="list-reset flex flex-row md:flex-col pt-3 md:py-3 px-1 md:px-2 text-center md:text-left">
+                              {
+                                dashboard_menu_items.iter().map(|item| html!{
+                                    <li class="mr-3 flex-1">
+                                        <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
+                                        <i class="fas fa-tasks pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">{item.title.clone()}</span>
+                                        </a>
+                                    </li>
+                                }).collect::<Html>()
+                              }
                               <li class="mr-3 flex-1">
                                   <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
                                       <i class="fas fa-tasks pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">{"Tasks"}</span>
