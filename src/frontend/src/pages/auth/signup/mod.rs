6f9a702135fb7965abd4f789110dd3e406bd4fc8
vilayat-ali <vilayatcodemysite@gmail.com>
2023-05-26 12:33:45 +0530

    Added successful form handling on signup form

--- src/frontend/src/pages/auth/signup/mod.rs
+++ src/frontend/src/pages/auth/signup/mod.rs
@@ -1,8 +1,7 @@
 use gloo_console::log;
-use wasm_bindgen::{JsCast, UnwrapThrowExt};
+use wasm_bindgen::JsCast;
 use web_sys::{HtmlInputElement, InputEvent};
 use yew::{
-    html::onchange::Event,
     prelude::{function_component, html, Html},
     use_state, Callback,
 };
@@ -13,6 +12,14 @@ use crate::layout::website::WebsiteLayout;
 pub fn Signup() -> Html {
     let email = use_state(|| String::new());
     let password = use_state(|| String::new());
+
+    let oninput_username = Callback::from({
+        let username = username.clone();
+        move |input_event: InputEvent| {
+            let target: HtmlInputElement = input_event.target().unwrap().dyn_into().unwrap();
+            username.set(target.value());
+        }
+    });
 
     let oninput_email = Callback::from({
         let email = email.clone();
@@ -31,7 +38,12 @@ pub fn Signup() -> Html {
     });
 
     let handle_submit = Callback::from(move |_| {
-        log!(format!("{}, {}", &*email.clone(), &*password.clone()));
+        log!(format!(
+            "{}, {}, {}",
+            &*username.clone(),
+            &*email.clone(),
+            &*password.clone()
+        ));
     });
 
     html! {
@@ -40,6 +52,21 @@ pub fn Signup() -> Html {
                 <div class="bg-white text-gray-800 dark:text-white dark:bg-gray-700 p-10 rounded-lg shadow-lg">
                     <h1 class="text-2xl font-semibold mb-6">{"Signup"}</h1>
                     <form>
+                    <div class="mb-4">
+                        <label
+                        class="block text-gray-800 dark:text-white font-bold mb-2"
+                        for="Username"
+                        >
+                        {"Username"}
+                        </label>
+                        <input
+                        oninput={oninput_username}
+                        class="appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-200 leading-tight focus:outline-none focus:shadow-outline"
+                        id="Username"
+                        type="text"
+                        placeholder="Username"
+                        />
+                    </div>
                     <div class="mb-4">
                         <label
                         class="block text-gray-800 dark:text-white font-bold mb-2"
